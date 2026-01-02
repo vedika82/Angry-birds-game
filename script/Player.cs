f35986a493c5fd22991b5773ed41baf98f534ea8
vedika82 <anandvedika420@gmail.com>
2026-01-02 19:25:05 +0530

    dragging, changing its colour on holding and launching basically setting problem changed to new and old both

--- script/Player.cs
+++ script/Player.cs
@@ -5,6 +5,11 @@ public class Player : MonoBehaviour
 //if public it show in the inspector and can be modified 
 private Rigidbody2D rb; 
 private SpriteRenderer sr;
+private Vector2 startPosition;
+private Vector2 currentPosition;
+private Vector2 direction;  
+public float launchSpeed;
+public float maxDragAmount;
 
 // Start is called once before the first execution of Update after the MonoBehaviour is created
 void Start()
@@ -13,33 +18,71 @@ void Start()
     rb.isKinematic = true;
 
     sr=GetComponent<SpriteRenderer>();
+    startPosition = rb.position;
+
+
+
+     Debug.Log("START is running");
 }
 
 // Update is called once per frame
 void Update()
 {
-    
 }
-
 
 //to change color on mouse click of the bird
 private void OnMouseDown()
 {
+    Debug.Log("My script in OnMouseDown is running");
     sr.color = Color.red;
 }
 
 
-    private void UpMouseDown()
+private void OnMouseUp()
 {
+    Debug.Log("My script in OnMouseUp is running");
+    currentPosition=rb.position;//from a position to another position here done this to make it stay there
+    direction = startPosition - currentPosition;
+    direction.Normalize();
+
+    rb.isKinematic = false;
+    rb.AddForce(direction * launchSpeed);//to add force in the direction we want to launch  
+
+
     sr.color = Color.white;
+
 }
 
 
 private void OnMouseDrag()
 {
     //to get the mouse position in world space
-    Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);    
+    Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition); 
+    // Vector2 desiredPosition = mousePosition; //we are intializing desired position to mouse position
+    
+    // float distance = Vector2.Distance(desiredPosition, startPosition);
+    // if (distance>maxDragAmount)
+    //     {
+    //         direction = desiredPosition - startPosition;
+    //         direction.Normalize();
+    //         desiredPosition = startPosition + (direction * maxDragAmount);
+        // }
+
+
+
+
+
+     //so that we cannot move outside the 2d space
+    // if (desiredPosition.x>startPosition.x)
+    // {
+    //     desiredPosition.x=startPosition.x;
+    // }
     //as we know that there is z axis in camera but not in 2d objects therefore it is required to take transform by hand of the user and not use the mouse z axis
+    // dont need this
     transform.position=new Vector3(mousePosition.x,mousePosition.y,transform.position.z);
+
+    // rb.position=desiredPosition;
+
+
 }
 }
