5e1fae614b4057df2d82a28912913b5442421c97
vedika82 <anandvedika420@gmail.com>
2026-01-04 01:25:20 +0530

    now dragging and hitting the boxes in game somthing wrong in physics will see

--- script/Player.cs
+++ script/Player.cs
@@ -1,4 +1,5 @@
 
+using System.Collections.ObjectModel;
 using UnityEngine; 
 public class Player : MonoBehaviour 
 { //private cause we dont want anyone to acccess outside the class 
@@ -43,6 +44,11 @@ private void OnMouseUp()
     Debug.Log("My script in OnMouseUp is running");
     currentPosition=rb.position;//from a position to another position here done this to make it stay there
     direction = startPosition - currentPosition;
+    Debug.Log("Direction: " + direction);
+    Debug.Log("startPosition: " + startPosition);
+    Debug.Log("currentPosition: " + currentPosition);
+
+
     direction.Normalize();
 
     rb.isKinematic = false;
@@ -58,31 +64,34 @@ private void OnMouseDrag()
 {
     //to get the mouse position in world space
     Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition); 
-    // Vector2 desiredPosition = mousePosition; //we are intializing desired position to mouse position
+    Vector2 desiredPosition = mousePosition; 
+    //we are intializing desired position to mouse position //vector2 have x and y only
     
-    // float distance = Vector2.Distance(desiredPosition, startPosition);
-    // if (distance>maxDragAmount)
-    //     {
-    //         direction = desiredPosition - startPosition;
-    //         direction.Normalize();
-    //         desiredPosition = startPosition + (direction * maxDragAmount);
-        // }
+    //vector2.distance distance between two points
+    float distance = Vector2.Distance(desiredPosition, startPosition);
+    if (distance>maxDragAmount)
+        {
+            direction = desiredPosition - startPosition;
+            direction.Normalize();
+            desiredPosition = startPosition + (direction * maxDragAmount);
+        }
 
 
 
 
 
      //so that we cannot move outside the 2d space
-    // if (desiredPosition.x>startPosition.x)
-    // {
-    //     desiredPosition.x=startPosition.x;
-    // }
+    if (desiredPosition.x>startPosition.x)
+    {
+        desiredPosition.x=startPosition.x;
+    }
     //as we know that there is z axis in camera but not in 2d objects therefore it is required to take transform by hand of the user and not use the mouse z axis
     // dont need this
-    transform.position=new Vector3(mousePosition.x,mousePosition.y,transform.position.z);
+    // transform.position=new Vector3(mousePosition.x,mousePosition.y,transform.position.z);
 
-    // rb.position=desiredPosition;
+    rb.position=desiredPosition;
 
 
 }
+
 }
